b3fee915295bf184683ecb8984956f8f457a8df5
Olivier Vignaud <ovi@ubiqube.com>
2023-03-29 14:49:02 +0200

    vim-k8s: Remove duplicate fluxrest.

--- mano-vim-k8s/src/main/java/com/ubiqube/etsi/mano/vim/k8s/HelmV3WrapperClient.java
+++ mano-vim-k8s/src/main/java/com/ubiqube/etsi/mano/vim/k8s/HelmV3WrapperClient.java
@@ -31,6 +31,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ubiqube.etsi.mano.dao.mano.config.Servers;
 import com.ubiqube.etsi.mano.dao.mano.k8s.K8sServers;
+import com.ubiqube.etsi.mano.service.rest.FluxRest;
 import com.ubiqube.etsi.mano.service.vim.VimException;
 import com.ubiqube.etsi.mano.service.vim.k8s.K8sClient;
 
