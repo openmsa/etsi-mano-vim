e5c6e588caa7ce4fd3cc290aa2da26bb2ad4869e
Olivier Vignaud <ovi@ubiqube.com>
2024-05-21 09:58:17 +0200

    🧪 k8s: Add event tests.

---
+++ mano-vim-k8s/src/test/java/com/ubiqube/etsi/mano/vim/k8s/event/ClusterCreateEventHandlerTest.java
@@ -0,0 +1,87 @@
+/**
+ *     Copyright (C) 2019-2024 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.vim.k8s.event;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+
+import io.x_k8s.cluster.v1beta1.Cluster;
+import io.x_k8s.cluster.v1beta1.ClusterStatus;
+import io.x_k8s.cluster.v1beta1.clusterstatus.Conditions;
+
+class ClusterCreateEventHandlerTest {
+	ClusterCreateEventHandler createService() {
+		return new ClusterCreateEventHandler();
+	}
+
+	@Test
+	void test() {
+		final ClusterCreateEventHandler srv = createService();
+		srv.onAdd(null);
+		srv.onDelete(null, false);
+		final Cluster cluster = new Cluster();
+		cluster.setStatus(new ClusterStatus());
+		cluster.getStatus().setConditions(List.of());
+		srv.onUpdate(cluster, cluster);
+		assertTrue(true);
+	}
+
+	@Test
+	void testBadType() {
+		final ClusterCreateEventHandler srv = createService();
+		srv.onAdd(null);
+		srv.onDelete(null, false);
+		final Cluster cluster = new Cluster();
+		cluster.setStatus(new ClusterStatus());
+		final Conditions cond = new Conditions();
+		cluster.getStatus().setConditions(List.of(cond));
+		srv.onUpdate(cluster, cluster);
+	}
+
+	@Test
+	void testFalse() {
+		final ClusterCreateEventHandler srv = createService();
+		srv.onAdd(null);
+		srv.onDelete(null, false);
+		final Cluster cluster = new Cluster();
+		cluster.setStatus(new ClusterStatus());
+		final Conditions cond = new Conditions();
+		cond.setType("Ready");
+		cond.setStatus("False");
+		cluster.getStatus().setConditions(List.of(cond));
+		srv.onUpdate(cluster, cluster);
+	}
+
+	@Test
+	void testTrue() throws InterruptedException {
+		final ClusterCreateEventHandler srv = createService();
+		srv.onAdd(null);
+		srv.onDelete(null, false);
+		final Cluster cluster = new Cluster();
+		cluster.setStatus(new ClusterStatus());
+		final Conditions cond = new Conditions();
+		cond.setType("Ready");
+		cond.setStatus("True");
+		cluster.getStatus().setConditions(List.of(cond));
+		srv.onUpdate(cluster, cluster);
+		srv.await(10, TimeUnit.SECONDS);
+	}
+}
---
+++ mano-vim-k8s/src/test/java/com/ubiqube/etsi/mano/vim/k8s/event/ClusterDeleteEventHandlerTest.java
@@ -0,0 +1,39 @@
+/**
+ *     Copyright (C) 2019-2024 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.vim.k8s.event;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+
+import io.x_k8s.cluster.v1beta1.Cluster;
+
+class ClusterDeleteEventHandlerTest {
+
+	@Test
+	void test() throws InterruptedException {
+		final ClusterDeleteEventHandler srv = new ClusterDeleteEventHandler();
+		srv.onAdd(null);
+		srv.onUpdate(null, null);
+
+		final Cluster obj = new Cluster();
+		srv.onDelete(obj, false);
+		srv.await(10, TimeUnit.SECONDS);
+
+	}
+
+}
